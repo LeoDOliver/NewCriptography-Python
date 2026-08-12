27f3319c8ad8221b671d0efe374ac773d070b56e
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2026-08-12 18:09:22 -0300

    Update UDPServer.py

--- UDPServer.py
+++ UDPServer.py
@@ -1,1 +1,10 @@
+import socket
 
+host = "127.0.0.1"
+port = 4444
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+cliente.sendto("AAAAAAAAAAAAAA".encode(),(host, port))
+
+dados, endereço = cliente.recvfrom(4096)
+print(dados.decode())
